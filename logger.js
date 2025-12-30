25cb54c441c66e803acb61b2489e9b5de86957ce
asepkikiabdulaziz <asepkiki.abdulaziz@gmail.com>
2025-12-30 18:38:15 +0700

    feat: initial commit - core application only

--- logger.js
+++ logger.js
@@ -62,3 +62,4 @@ const logger = {
 
 export { logger, isDevelopment };
 
+
--- run-server-simple.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Starting HTTP Server...
-echo Server akan berjalan di: http://localhost:8080
-echo Tekan Ctrl+C untuk menghentikan
-echo.
-npx --yes http-server -p 8080 -a localhost --cors
-pause
-
